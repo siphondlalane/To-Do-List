99ace784ebc4fddc5fd4953e067ddfde813c1db2
S Ndlalane <218070302@tut4life.ac.za>
2022-07-07 14:40:40 +0200

    first commit

---
+++ to_do_list.py
@@ -0,0 +1,26 @@
+
+import tkinter
+import tkinter.messagebox
+
+root = tkinter.Tk()
+root.title("\t\tTo Do List")
+
+
+def add_task():
+    task = edit.get()
+    listbox.insert(tkinter.END, task)
+
+listbox = tkinter.Listbox(root, height = 15, width = 50)
+listbox.pack()
+
+edit = tkinter.Entry(root, width = 50)
+edit.pack()
+
+button_add = tkinter.Button(root, text = "Add Task",width = 48, command = add_task)
+button_add.pack()
+
+root.mainloop()
+
+
+
+
